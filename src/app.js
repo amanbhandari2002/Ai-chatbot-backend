ca1f689ea4815bf2b1cf9c3c19591fb9cb904021
Aman bhandari <amanbhandari2002@gmail.com>
2023-11-28 02:04:47 +0530

    tried to fix database connectivity error

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import cors from "cors"
 
 config();
 const app=express();
+// app.use(cors({origin:"http://localhost:5173",credentials:true}))
 app.use(cors({origin:"https://aigptbotlic.netlify.app",credentials:true}))
 app.use(cookieParser(process.env.COOKIE_SECURITY,))
 app.use(express.json());
--- src/db/connection.js
+++ src/db/connection.js
@@ -3,14 +3,16 @@ import mongoose from 'mongoose';
 
 async function connectToDB() {
     try { 
+        
         await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
+        }
+        );
        
     }
     catch (err) {
-        // console.log(process.env.MONGODB_URL)
+        
         throw new Error("cannot connect");
     }
 }
