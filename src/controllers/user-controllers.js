38ebfbae23425187937a232af8d02c0cdf57b7ea
Aman bhandari <amanbhandari2002@gmail.com>
2023-11-29 00:12:13 +0530

    cookie error fixed

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -1,87 +1,86 @@
 import User from "../models/user.js";
-import {hash, compare} from "bcrypt"
+import { hash, compare } from "bcrypt"
 import tokenGenerator from "../utils/tokens-manager.js";
 
 
-const getAllUsers=async (req,res,next)=>{
+const getAllUsers = async (req, res, next) => {
     // get all users from the database
 
-    try{
-        const users= await User.find();
-        return res.status(200).json({message:"OK",users});
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "OK", users });
     }
-    catch(err){
-        return res.status(200).json({message:"error",cause:err.message});;
+    catch (err) {
+        return res.status(200).json({ message: "error", cause: err.message });;
     }
 }
 
 
 
-const signUp=async (req,res)=>{
+const signUp = async (req, res) => {
     // add new user to the database...
 
-    try{
-      
-        const {name,email,password}=req.body;
-        const existingUser= await User.findOne({email})
-        if(existingUser){
-            return res.status(422).json({"error":"user already exist"});
-        }
-        res.clearCookie("auth_token", {httpOnly:true, signed:true,path:"/",domain:".aibotbackend.onrender.com"})
-        const hashPassowrd= await hash(password,10);
+    try {
 
-        const user = new User({name,email,password:hashPassowrd});
+        const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email })
+        if (existingUser) {
+            return res.status(422).json({ "error": "user already exist" });
+        }
+        res.clearCookie("auth_token", { httpOnly: true, signed: true, path: "/", domain: ".aibotbackend.onrender.com", sameSite: 'None' })
+
+        const hashPassowrd = await hash(password, 10);
+
+        const user = new User({ name, email, password: hashPassowrd });
         user.save()
 
-        const token= tokenGenerator(user._id.toString(),user.email,"7d")
+        const token = tokenGenerator(user._id.toString(), user.email, "7d")
+        const expiretime = new Date();
+        var date = expiretime.getDate();
+        var expireTime = date + 7;
+        expiretime.setDate(expireTime);
+        res.cookie("auth_token", token, { path: '/', httpOnly: true, signed: true, expires: expiretime, domain: ".aibotbackend.onrender.com", sameSite: 'None' })
+
+        return res.status(200).json({ "message": "OK", user, email })
+    }
+
+
+    catch (err) {
+        return res.status(200).json({ message: "error", cause: err.message });;
+    }
+}
+
+
+const login = async (req, res) => {
+
+    const { email, password } = req.body;
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        return res.status(404).json({ "error": "user not registered" })
+    }
+
+    res.clearCookie("auth_token", { httpOnly: true, signed: true, path: "/", domain: ".aibotbackend.onrender.com", sameSite: 'None' })
+
+    const verifyPassword = await compare(password, user.password)
+    if (verifyPassword == false) {
+        return res.status(400).json({ "error": "password was incorect" })
+    }
+
+    const token = tokenGenerator(user._id.toString(), user.email, "7d")
     const expiretime = new Date();
     var date = expiretime.getDate();
     var expireTime = date + 7;
     expiretime.setDate(expireTime);
-    res.cookie("auth_token",token,{path:'/',httpOnly:true, signed:true,expires:expiretime,domain:".aibotbackend.onrender.com"})
-
-    return res.status(200).json({"message":"OK",user,email})
-    }
-
-    
-    catch(err){
-        return res.status(200).json({message:"error",cause:err.message});;
-    }
+    res.cookie("auth_token", token, { path: '/', httpOnly: true, signed: true, expires: expiretime, domain: ".aibotbackend.onrender.com", sameSite: 'None' })
+    return res.status(200).json({ "message": "found", email, user })
 }
 
 
-const login=async (req,res)=> {
-
-    const {email,password}=req.body;
-    const user = await User.findOne({email})
-    
-    if(!user){
-        return res.status(404).json({"error":"user not registered"})
-    }
-
-    // res.clearCookie("auth_token", {httpOnly:true, signed:true,path:"/",domain:".aibotbackend.onrender.com"})
-
-    const verifyPassword= await compare(password,user.password)
-    if(verifyPassword==false){
-        return res.status(400).json({"error":"password was incorect"})
-    }
-    console.log('wtffffffff')
-    const token= tokenGenerator(user._id.toString(),user.email,"7d")
-    console.log(token,'token')
-    const expiretime = new Date();
-    var date = expiretime.getDate();
-    var expireTime = date + 7;
-    expiretime.setDate(expireTime);
-    res.cookie("auth_token",token,{maxAge: 900000})
-    console.log(req.signedCookies)
-    return res.status(200).json({"message":"found",email,user})
-}
-
-
-const logout=async (req,res)=>{
-    res.clearCookie("auth_token", {httpOnly:true, signed:true,path:"/",domain:".aibotbackend.onrender.com"})
-    res.status(200).json({"message":"logout successful"})
+const logout = async (req, res) => {
+    res.cookie("auth_token", token, { path: '/', httpOnly: true, signed: true, expires: expiretime, domain: ".aibotbackend.onrender.com", sameSite: 'None' })
+    res.status(200).json({ "message": "logout successful" })
 
 }
 
-export  {getAllUsers, signUp,login,logout};
+export { getAllUsers, signUp, login, logout };
