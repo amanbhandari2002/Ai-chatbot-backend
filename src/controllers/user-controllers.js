16c1a0e1d2d3479d3a6f50faf169e121d9424c02
Aman bhandari <amanbhandari2002@gmail.com>
2023-11-28 17:03:51 +0530

    one more tryyyy

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -72,7 +72,7 @@ const login=async (req,res)=> {
     var date = expiretime.getDate();
     var expireTime = date + 7;
     expiretime.setDate(expireTime);
-    res.cookie("auth_token",token,{httpOnly:true, signed:true,maxAge: 900000})
+    res.cookie("auth_token",token,{maxAge: 900000})
     console.log(req.signedCookies)
     return res.status(200).json({"message":"found",email,user})
 }
